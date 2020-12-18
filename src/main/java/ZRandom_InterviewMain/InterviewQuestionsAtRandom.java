d55383c95820abb8d077f5e1dd4775c35471cfc4
Stephanie <stephanie.galvez.c@gmail.com>
2020-12-18 13:46:55 -0500

    Interview questions proposed features

--- src/main/java/ZRandom_InterviewMain/InterviewQuestionsAtRandom.java
+++ src/main/java/ZRandom_InterviewMain/InterviewQuestionsAtRandom.java
@@ -1,5 +1,16 @@
 package ZRandom_InterviewMain;
 
+//Author: Stephanie G.
+
+/*
+  Propose new features:
+   1. add a timer feature to the program.
+      1.1. adjust timer to reflect complexity of the questions, as some questions might take 1 to 2 min to answers, while others might take up to 5 min.
+   2. Combine group28Names class so both programs functionalities can run at the same time
+   3. Create an arrayList that stores questions that have been utilized twice
+ */
+
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -23,7 +34,7 @@ public class InterviewQuestionsAtRandom {
 
         ArrayList<String> tempInterviewQuestions = new ArrayList<>();
 
-        for(int i =0;i<3;i++) {
+        for(int i =0;i<1;i++) {
             int randInt = rand1.nextInt(interviewQuestions.size());//initializing a random number using our RandomClass object "rand"
             tempInterviewQuestions.add(interviewQuestions.get(randInt));//adds the element in the random index that it generated
             interviewQuestions.remove(randInt);//removes the element that was just chosen.
