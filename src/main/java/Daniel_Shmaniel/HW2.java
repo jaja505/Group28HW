a223ff6f1e9fed47c67f79eb86d92f5eae2483f3
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-12-17 20:35:28 -0500

    added a question to InterviewQuestions.txt started working on HW2.java

--- src/main/java/Daniel_Shmaniel/HW2.java
+++ src/main/java/Daniel_Shmaniel/HW2.java
@@ -1,7 +1,28 @@
 package Daniel_Shmaniel;
+import AsignmentUtils.Population.Person;
+import AsignmentUtils.Population.Population;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HW2 {
     public static void main(String[] args) {
-        //TGHIS IS JUST FOR PUSH
+        ArrayList<Person> allPeople = new ArrayList<>(Arrays.asList(Population.createPopulation(200)));
+
+    /*
+    1)  a - Create ArrayList of Person type and name it singlePeople
+        b - Add all single people from allPeople to singlePeople (hint: isMarried = false)
+
+    2)  a - Create ArrayList of Person type and name it parents
+        b - Add all people that have kids from allPeople to parents (hint: numbOfChildren > 0)
+
+    3)  a - Create ArrayList of Person type and name it noEngineers
+        b - Add all people from allPeople to noEngineers
+        c - remove all people that are "software engineer" (hint: occupation.equals("software engineer"))
+        Very optional:
+        d - if any relatives associated remove them too. (hint: look out for spouseId and childId)
+
+    4)  a - change occupation from "doctor" to "Artist" in allPeople
+     */
     }
 }
