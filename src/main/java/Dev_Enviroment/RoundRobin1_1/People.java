e96ad05327b59c3fd702d393c666a24534483236
Livio Daljai <livio.daljani@gmail.com>
2020-12-19 01:06:16 -0500

    Created text file GroupNames, created a assembleGroup() method in People class, tested method in RR Main

--- src/main/java/Dev_Enviroment/RoundRobin1_1/People.java
+++ src/main/java/Dev_Enviroment/RoundRobin1_1/People.java
@@ -1,11 +1,29 @@
 package Dev_Enviroment.RoundRobin1_1;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class People {
 
 
 
 
-    public static void main(String[] args) {
-        //123
+
+    public static ArrayList assembleGroup() throws FileNotFoundException {
+        Random rand = new Random();
+        FileInputStream file = new FileInputStream("src/main/java/Dev_Enviroment/RoundRobin1_1/GroupNames");//created a fileStream Object "File" that we will be reading.
+
+        Scanner sc = new Scanner(file);//make a scanner specifically to read that file...NOT SYSTEM.IN
+
+        ArrayList<String> groupNamesList = new ArrayList<>(); // making an arrayList to put all our names in
+        while (sc.hasNext()) {
+            groupNamesList.add(sc.nextLine());
+        }
+       return groupNamesList;
     }
+
+
 }
--- src/main/java/Dev_Enviroment/RoundRobin1_1/RoundRobin_Main.java
+++ src/main/java/Dev_Enviroment/RoundRobin1_1/RoundRobin_Main.java
@@ -1,25 +1,14 @@
 package Dev_Enviroment.RoundRobin1_1;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class RoundRobin_Main {
 
-    public static void main(String[] args) {
-        //each question
+    public static void main(String[] args) throws FileNotFoundException {
 
-        ArrayList<Question> questionArrayList = new ArrayList<>();
-        Question q1 = new Question("Question 1");
-        for (int i = 0; i < 10; i++) {
-            questionArrayList.add(new Question("Question".concat((i + 1) + "")));
-            System.out.println(questionArrayList.get(i));
-        }
-
-        //for making the count;
-        q1.questionAnswered();
-        q1.questionAnswered();
-        if (q1.isSolvedCount == 2) {//for removing from list
-            System.out.println(q1.isSolvedCount);
-        }
+        ArrayList<People> group = People.assembleGroup();
+        System.out.println(group);
 
 
     }
