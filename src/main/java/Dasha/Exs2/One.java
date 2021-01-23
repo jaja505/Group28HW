18ce25db2f15bb1c77a434ad62367a6710d28e1c
volkaud <daria.volkau@gmail.com>
2021-01-23 13:18:30 -0500

    Added TestNG and JavaFaker dependencies. Dasha

--- src/main/java/Dasha/Exs2/One.java
+++ src/main/java/Dasha/Exs2/One.java
@@ -1,6 +1,29 @@
 package Dasha.Exs2;
 
-public class One {
 
-}
+
+public class One {
+//TC#1: filter movies by rating
+    //1. Open Chrome browser
+
+
+    }
+    //2. Go to https://www.imdb.com/list/ls026253657/
+    //3. Verify  title
+    //Expected: "Movies 2020 - IMDb"
+    //4. collect all similar WebElements containing movie info on to List
+    //5. remove WebElement with movie rating less than 6
+    //6. create HTML file
+    //7. collect all WebElements from inside <head></head>:
+    // target: <link rel="stylesheet" type="text/css">
+    // and place them to your HTML file inside <head></head>
+    //8. create next element inside <body></body>
+    // target:
+    // <div class="lister list detail sub-list">
+    //    <div class="lister-list"></div>
+    // </div>
+    //9. place all WebElemens with movie info inside:
+    // <div class="lister-list"></div>
+    // 10. open your THML file with driver
+
 
